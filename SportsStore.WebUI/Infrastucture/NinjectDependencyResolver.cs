ff11880fe296a1cd594083b68939c689802c3763
kono1love <kono1love@yandex.ru>
2016-10-20 18:02:05 +0400

    Init SportsStore solution. Start chapter 7

---
+++ SportsStore.WebUI/Infrastucture/NinjectDependencyResolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web.Mvc;
+using Ninject;
+using  Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+
+namespace SportsStore.WebUI.Infrastucture
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviveType)
+        {
+            return kernel.TryGet(serviveType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //pull Bindings heremoc
+            Mock<IProductRepository> mock = new Mock<IProductRepository>(); 
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Football", Price = 25},
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+            });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
